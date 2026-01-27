97e89a4ac78c6b33611d96902257b5e648179f4d
Aaron <smlife202@gmail.com>
2026-01-27 17:23:10 +0900

    Simple Carrot Market UI Created

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,73 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Center(child: Text("Project Init")),
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color(0xfff4f4f4),
+          title: Text("App"),
+          actions: [
+            Align(
+              alignment: Alignment.topRight,
+              child: Container(
+                width: 250,
+                height: double.infinity,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Icon(Icons.search),
+                    Icon(Icons.menu),
+                    Icon(Icons.notification_add),
+                  ],
+                ),
+              ),
+            )
+          ],
+          ),
+        body: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border.all(color: Colors.black)
+
+          ),
+          
+          width: double.infinity,
+          height: 200,
+          margin: EdgeInsets.all(10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Container(
+                width: 150,
+                child: Image.asset('2.jpg'),
+              ),
+              Container(
+                width: 250,
+                height: double.infinity,
+                margin: EdgeInsets.all(5),
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Text 1", style: TextStyle(fontSize: 30),),
+                    Text("Text 2"),
+                    Text("Text 3"),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Icon(Icons.favorite_outline),
+                        Text("4")
+                      ],
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+
+        ),
+      ),
     );
   }
 }
