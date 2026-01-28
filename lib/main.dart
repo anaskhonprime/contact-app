33cd5c8d39384f779593e0c97912280b7e3a9e57
Aaron <smlife202@gmail.com>
2026-01-28 17:40:59 +0900

    StatefulWidget, state vs normal variable, ListView.builder

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,36 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var a = 1;
+  var name = [
+    {'김민이': 0},
+    {'김용전': 0},
+    {'치킨집': 0},
+  ];
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          child: Text(a.toString()),
+          onPressed: () {
+            setState(() {});
+          },
+        ),
+
         appBar: AppBar(
           backgroundColor: Color(0xfff4f4f4),
           title: Text("App"),
@@ -31,51 +50,42 @@ class MyApp extends StatelessWidget {
                   ],
                 ),
               ),
-            )
+            ),
           ],
-          ),
-        body: Container(
-          decoration: BoxDecoration(
-            color: Colors.white,
-            border: Border.all(color: Colors.black)
-
-          ),
-          
-          width: double.infinity,
-          height: 200,
-          margin: EdgeInsets.all(10),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Container(
-                width: 150,
-                child: Image.asset('2.jpg'),
+        ),
+        body: Row(
+          children: [
+            Flexible(
+              flex: 7,
+              child: ListView.builder(
+                itemCount: 3,
+                itemBuilder: (context, i) {
+                  return ListTile(
+                    leading: Text(name[i].values.first.toString()),
+                    title: Text(name[i].keys.first),
+                  );
+                },
               ),
-              Container(
-                width: 250,
-                height: double.infinity,
-                margin: EdgeInsets.all(5),
-                padding: EdgeInsets.all(10),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text("Text 1", style: TextStyle(fontSize: 30),),
-                    Text("Text 2"),
-                    Text("Text 3"),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Icon(Icons.favorite_outline),
-                        Text("4")
-                      ],
-                    ),
-                  ],
-                ),
-              )
-            ],
-          ),
+            ),
 
+            Flexible(
+              flex: 3,
+              child: Column(
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      setState(() {
+                        
+                      });
+                    },
+                    child: Text("+"),
+                  ),
+                  TextButton(onPressed: () {}, child: Text("+")),
+                  TextButton(onPressed: () {}, child: Text("+")),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
